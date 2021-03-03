ebf7f7aa112329e91b06e4d07899a5707e64c25d
Biplab21 <biplabpaul569@gmail.com>
2021-03-04 01:42:03 +0530

    initial deploy

--- resp.js
+++ resp.js
@@ -1,8 +1,6 @@
 burger = document.querySelector(".hamburger");
 navbar = document.querySelector(".navbar");
-logo = document.querySelector(".logo");
 
 burger.addEventListener('click', () => {
-    logo.classList.toggle('v_class');
     navbar.classList.toggle('h_nav');
 });
